39a150e4b9eb8627b9e1b3908900a71058e84b80
mehrdad-dev <mehrdad.dev@yahoo.com>
2020-10-08 23:47:55 +0330

    add loading

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,52 +1,52 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 
-    // Smooth on external page
-    $(function() {
-      setTimeout(function() {
-        if (location.hash) {
-          /* we need to scroll to the top of the window first, because the browser will always jump to the anchor first before JavaScript is ready, thanks Stack Overflow: http://stackoverflow.com/a/3659116 */
-          window.scrollTo(0, 0);
-          target = location.hash.split('#');
-          smoothScrollTo($('#'+target[1]));
-        }
-      }, 1);
+	// Smooth on external page
+	$(function () {
+		setTimeout(function () {
+			if (location.hash) {
+				/* we need to scroll to the top of the window first, because the browser will always jump to the anchor first before JavaScript is ready, thanks Stack Overflow: http://stackoverflow.com/a/3659116 */
+				window.scrollTo(0, 0);
+				target = location.hash.split('#');
+				smoothScrollTo($('#' + target[1]));
+			}
+		}, 1);
 
-      // taken from: https://css-tricks.com/snippets/jquery/smooth-scrolling/
-      $('a[href*=\\#]:not([href=\\#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          smoothScrollTo($(this.hash));
-          return false;
-        }
-      });
+		// taken from: https://css-tricks.com/snippets/jquery/smooth-scrolling/
+		$('a[href*=\\#]:not([href=\\#])').click(function () {
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+				smoothScrollTo($(this.hash));
+				return false;
+			}
+		});
 
-      function smoothScrollTo(target) {
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		function smoothScrollTo(target) {
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-        }
-      }
-    });
-	
-	
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+			}
+		}
+	});
+
+
 	// toggle comments
-    $('.show-comments').on('click', function() {  
-		$('#comments').toggleClass('comments--show');		
+	$('.show-comments').on('click', function () {
+		$('#comments').toggleClass('comments--show');
 	});
 
 	//toggle search
-	$('.show-search').on('click', function() {  
-		$('.bd-search').toggleClass('search--show');		
+	$('.show-search').on('click', function () {
+		$('.bd-search').toggleClass('search--show');
 	});
-    
-    // spoilers
-     $(document).on('click', '.spoiler', function() {
-        $(this).removeClass('spoiler');
-     });
-    
- });   
+
+	// spoilers
+	$(document).on('click', '.spoiler', function () {
+		$(this).removeClass('spoiler');
+	});
+
+});
 
 // deferred style loading
 var loadDeferredStyles = function () {
@@ -65,54 +65,54 @@ else window.addEventListener('load', loadDeferredStyles);
 
 
 // Reset animations on page: body.preload
-setTimeout(function(){
-	document.body.className="";
-},500);
+setTimeout(function () {
+	document.body.className = "";
+}, 500);
 
 // Open/close navigation when clicked .nav-icon
-$(document).ready(function(){
-	$('.nav-icon').click(function(){
+$(document).ready(function () {
+	$('.nav-icon').click(function () {
 		$('.nav-icon').toggleClass('active');
 	});
-	$(".nav-icon").click(function(){
+	$(".nav-icon").click(function () {
 		$("#menu").toggleClass('active');
 	});
-	$(".nav-icon").click(function(){
+	$(".nav-icon").click(function () {
 		$("#blackover-nav").toggleClass('active');
 	});
-	$(".nav-icon").click(function(){
+	$(".nav-icon").click(function () {
 		$("body").toggleClass('active-side');
 	});
 });
 
 // Close navigation when clicked .blackover (Black background)
-$(document).ready(function(){
-	$("#blackover-nav").click(function(){
+$(document).ready(function () {
+	$("#blackover-nav").click(function () {
 		$(".nav-icon").removeClass('active');
 	});
-	$("#blackover-nav").click(function(){
+	$("#blackover-nav").click(function () {
 		$("#menu").removeClass('active');
 	});
-	$("#blackover-nav").click(function(){
+	$("#blackover-nav").click(function () {
 		$("#blackover-nav").removeClass('active');
 	});
-	$("#blackover-nav").click(function(){
+	$("#blackover-nav").click(function () {
 		$("body").removeClass('active-side');
 	});
 });
 
 // Grid selector Inspiration
-$(document).ready(function(){
-	$(".grid-selector").click(function(){
+$(document).ready(function () {
+	$(".grid-selector").click(function () {
 		$(".grid-selector").toggleClass('active');
 	});
-	$(".grid-selector").click(function(){
+	$(".grid-selector").click(function () {
 		$(".post").toggleClass('active');
 	});
 });
 
-$(document).keyup(function(e) {
-	if (e.keyCode == 27) { 
+$(document).keyup(function (e) {
+	if (e.keyCode == 27) {
 		$(".nav-icon").removeClass('active');
 		$("#menu").removeClass('active');
 		$("#blackover-nav").removeClass('active');
@@ -123,12 +123,12 @@ $(document).keyup(function(e) {
 
 // remove all :hover stylesheets on mobile
 function hasTouch() {
-return 'ontouchstart' in document.documentElement
+	return 'ontouchstart' in document.documentElement
 		|| navigator.maxTouchPoints > 0
 		|| navigator.msMaxTouchPoints > 0;
 }
 
-if (hasTouch()) { 
+if (hasTouch()) {
 	try {
 		for (var si in document.styleSheets) {
 			var styleSheet = document.styleSheets[si];
@@ -142,43 +142,54 @@ if (hasTouch()) {
 				}
 			}
 		}
-	} catch (ex) {}
+	} catch (ex) { }
 }
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    //Check to see if the window is top if not then display button
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 300) {
-            $('.scroll-top').addClass('active');
-        } else {
-            $('.scroll-top').removeClass('active');
-        }
-    });
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 300) {
+			$('.scroll-top').addClass('active');
+		} else {
+			$('.scroll-top').removeClass('active');
+		}
+	});
 
-    //Click event to scroll to top
-    $('.scroll-top').click(function(){
-        $('html, body').animate({scrollTop : 0},300);
-        return false;
-    });
+	//Click event to scroll to top
+	$('.scroll-top').click(function () {
+		$('html, body').animate({ scrollTop: 0 }, 300);
+		return false;
+	});
 
 });
 
 
 // DOCS
 
-$(document).ready(function(){
-    
+$(document).ready(function () {
 
-     //Check to see if the back-menu is in the div
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 130) {
-            $('.back-page-button-dark').removeClass('back-page-button-w');
-        } else {
-            $('.back-page-button-dark').addClass('back-page-button-w');
-        }
-    });
+
+	//Check to see if the back-menu is in the div
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 130) {
+			$('.back-page-button-dark').removeClass('back-page-button-w');
+		} else {
+			$('.back-page-button-dark').addClass('back-page-button-w');
+		}
+	});
 
 
 });
+
+
+$(window).on('load', function(){
+	setTimeout(removeLoader, 500); //wait for page load PLUS two seconds.
+  });
+  function removeLoader(){
+	  $( "#loading" ).fadeOut(500, function() {
+		// fadeOut complete. Remove the loading div
+		$( "#loading" ).remove(); //makes page more lightweight 
+	});  
+}
