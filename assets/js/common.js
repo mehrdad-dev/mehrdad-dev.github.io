d3a9eb9100bffb40d5aa53afb6f99a8abf6eb0c7
mehrdad mohammadian <40211374+mehrdad-dev@users.noreply.github.com>
2022-08-17 21:50:53 +0430

    update js

--- assets/js/common.js
+++ assets/js/common.js
@@ -7,3 +7,14 @@ $(document).ready(function() {
     });
     $('.navbar-nav').find('a').removeClass('waves-effect waves-light');
 });
+
+
+// const headings = document.querySelectorAll('h1,h2,h3'); // 1
+// const linkContent = "<span style='font-size:20px; margin-left:10px;'>🔗</span>"; // 2
+
+// for (const heading of headings) { // 3
+//     const linkIcon = document.createElement('a'); // 4
+//     linkIcon.setAttribute('href', `#${heading.id}`); // 5
+//     linkIcon.innerHTML = linkContent; // 6
+//     heading.appendChild(linkIcon); // 7
+// }
